754b75b573184a728d53f1e828b9ad430210cc09
vinshavijayan <110905204+VinshaRookieNinja@users.noreply.github.com>
2023-07-31 10:33:11 +0400

    products page bg-overlay added

--- ecom_23_frontend/app/components/landing/index.js
+++ ecom_23_frontend/app/components/landing/index.js
@@ -4,9 +4,9 @@ export default function LandingSec({ title, desc, img }) {
   return (
     <>
       <div
-        className="h-[90vh] bg-cover bg-center   text-white flex flex-col justify-center px-5 md:px-32 "
+        className="h-[90vh] bg-cover bg-center text-white flex flex-col justify-center px-5 md:px-32"
         style={{
-          background: `linear-gradient(51deg, rgba(0,80,140,0.58) 0%, rgba(0,0,0,0.8) 100%), url(${img})`,
+          background: `linear-gradient(51deg, rgba(21, 167, 220,0.7) 0%, rgba(0,0,0,0.8) 100%), url(${img})`,
           backgroundSize: "cover",
           backgroundPosition: "left center",
         }}
